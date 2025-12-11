d121ed21027e84e177b168c784b28956afa84ccd
pramudia <sarcastrope@gmail.com>
2025-12-11 22:31:46 +0700

    style: remove unused import

--- src/main/java/test/avows/customer/exception/ApiExceptionHandler.java
+++ src/main/java/test/avows/customer/exception/ApiExceptionHandler.java
@@ -1,7 +1,5 @@
 package test.avows.customer.exception;
 
-import jakarta.annotation.Nullable;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
